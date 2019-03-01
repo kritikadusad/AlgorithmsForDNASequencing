3ad82e5b71ef9aaef8601c8440e7e6cfcf3d180d
Kritika Dusad <kritikadusad@gmail.com>
2019-03-01 09:20:36 -0800

    made function that uses pigeonhole priciple and k-mer index.

--- kmer_index.py
+++ kmer_index.py
@@ -5,6 +5,7 @@
 __author__ = "Ben Langmead"
 
 import bisect
+from bm_preproc import BoyerMoore
 
 
 class Index(object):
@@ -43,3 +44,69 @@ def readGenome(filename):
 
 t = readGenome("chr1.GRCh38.excerpt.fasta")
 index = Index(t, 8)
+
+
+def queryIndex(p, t, index):
+    k = index.k
+    offsets = []
+
+    for i in index.query(p):
+        if p[k:] == t[i + k:i + len(p)]:
+            offsets.append(i)
+    return offsets
+
+
+def pigeon_hole_index_matching(p, t, index, n):
+    """ 
+    n = maximum number of mismatches allowed
+    
+    """
+    segment_length = int(len(p) / (n + 1))
+    matches_set = set()
+
+    for i in range(n + 1):
+        start = i * segment_length
+        end = min((i + 1) * segment_length, len(p))
+
+        occurrences = queryIndex(p[start:end], t, index)
+
+        for o in occurrences:
+            if o < start or o - start + len(p) > len(t):
+                continue
+
+            mismatches = 0
+            for j in range(0, start):
+                if not p[j] == t[o - start + j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+            for j in range(end, len(p)):
+                if not p[j] == t[o - start + j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+            if mismatches <= n:
+                matches_set.add(o - start)
+        return len(list(matches_set))
+
+
+def naive_2mm(p, t):
+    occurences = []
+    for i in range(len(t) - len(p) + 1):
+        match = True
+        error = 0
+        for j in range(len(p)):
+            if t[i + j] != p[j]:
+                error += 1
+                if error > 2:
+                    match = False
+                    break
+        if match:
+            occurences.append(i)
+    return len(occurences)
+
+
+p = 'GGCGCGGTGGCTCACGCCTGTAAT'
+
+print(pigeon_hole_index_matching(p, t, index, n=2))
+print(naive_2mm(p, t))
